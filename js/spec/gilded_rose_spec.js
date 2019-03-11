41a0a96fa5b17c0e76ce9a17febc1ecf2b287a5a
Mario Quintana <pyrros2000@gmail.com>
2019-03-11 13:57:17 -0700

    Commented out all other tests and started with 'quality never over 50'. Making it  this far after starting off inccerctly, I believe I should conitinue writing tests to make sure that the output is correct. If a test continues to fail then it may be that I will have to work on the update_quality function and correct some logic?

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -6,4 +6,47 @@ describe("Gilded Rose", function() {
     expect(items[0].name).toEqual("foo");
   });
 
+  // it("Once the sell by date has passed, quality degrades twice as fast", function() {
+  //   items = [ new Item("foo", 0, 0) ];
+  //   update_quality();
+  //   expect(items[0].name).toEqual("foo");
+  // });
+
+  // it("The quality of an item is never negative", function() {
+  //   items = [ new Item("foo", 0, 0) ];
+  //   update_quality();
+  //   expect(items[0].name).toEqual("foo");
+  // });
+
+  // it("'Aged Brie' actually increases in quality the older it gets", function() {
+  //   items = [ new Item("foo", 0, 0) ];
+  //   update_quality();
+  //   expect(items[0].name).toEqual("foo");
+  // });
+
+  it("The quality of an item is never more than 50", function() {
+    items = [ new Item("foo", 4, 51) ];
+    update_quality();
+    expect(items[0].quality).toEqual(50);
+    expect(items[0].sell_in).toEqual(3);
+  });
+
+  // it("'Sulfuras', being a legendary item, never has to be sold or decreases in quality", function() {
+  //   items = [ new Item("foo", 0, 0) ];
+  //   update_quality();
+  //   expect(items[0].name).toEqual("foo");
+  // });
+
+  // it("'Backstage passes', like aged brie, increases in quality as it's sell-in value approaches; quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but quality drops to 0 after the concert", function() {
+  //   items = [ new Item("foo", 0, 0) ];
+  //   update_quality();
+  //   expect(items[0].name).toEqual("foo");
+  // });
+
+  // it("'Conjured' items degrade in quality twice as fast as normal items", function() {
+  //   items = [ new Item("foo", 0, 0) ];
+  //   update_quality();
+  //   expect(items[0].name).toEqual("foo");
+  // });
+
 });
